b3721230a83ea5999ce1de0352816f3038f09267
lyrain2001 <yliu217@u.rochester.edu>
2024-01-01 17:44:46 -0500

    add

--- hpc_scripts/stageB_fine_tune.sh
+++ hpc_scripts/stageB_fine_tune.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=mind-vis-stageA2_mbm_finetune
-#SBATCH --output=mind-vis-stageA2_mbm_finetune.out
-#SBATCH -p gpu
+#SBATCH --job-name=mind-vis-stageB2_ldm_finetune
+#SBATCH --output=mind-vis-stageB2_ldm_finetune.out
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
-#SBATCH -t 2-1:00:00 
-#SBATCH --mem=64g -C V100
+#SBATCH -t 3-1:00:00 
+#SBATCH --mem=64g
 
 conda activate mind-vis
 cd ..
