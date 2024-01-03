cc8924590ead367220cec05af747858424125a3b
lyrain2001 <yliu217@u.rochester.edu>
2024-01-03 05:53:41 -0500

    modify hpc script

--- hpc_scripts/stageB_fine_tune.sh
+++ hpc_scripts/stageB_fine_tune.sh
@@ -6,12 +6,12 @@
 #SBATCH -n 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
-#SBATCH -t 3-1:00:00 
+#SBATCH --gres=gpu:v100:1
+#SBATCH -t 7-0:00:00 
 #SBATCH --mem=64g
 
 conda activate mind-vis
 cd ..
-python code/stageB_ldm_finetune.py --dataset BOLD5000 --num_epoch 500 --batch_size 5
+python code/stageB_ldm_finetune.py --dataset BOLD5000 --num_epoch 500
 conda deactivate
 cd hpc_scripts
